993b8fcaf500213f73c093dbe0f974ac88e1d201
Andreas Nørregaard Pedersen <anpe53920@edu.ucl.dk>
2023-10-05 08:46:09 +0200

    removal of password

--- RecieverJPGManipulator/EmailService.cs
+++ RecieverJPGManipulator/EmailService.cs
@@ -34,7 +34,7 @@ namespace RecieverJPGManipulator
             mail.IsBodyHtml = true;
             smtpClient.Port = 587;
             smtpClient.UseDefaultCredentials = false;
-            smtpClient.Credentials = new System.Net.NetworkCredential("anpe53920@edu.ucl.dk", "Norregaard1!"); //replace password
+            smtpClient.Credentials = new System.Net.NetworkCredential("anpe53920@edu.ucl.dk", "password"); //replace password
             smtpClient.EnableSsl = true;
             smtpClient.Send(mail);
 
